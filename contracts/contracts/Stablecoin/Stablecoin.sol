98a1238097edc2a71bf0d6a9c8edf00a38cddf25
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-02-17 14:01:56 +0100

    feat(contracts): Stablecoin smart contract

---
+++ contracts/contracts/Stablecoin/Stablecoin.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Stablecoin is ERC20 {
+    constructor() ERC20("USDC", "USDC") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
