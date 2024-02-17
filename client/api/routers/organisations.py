1d4c28b321dd5996007462697f3a9aacc5f32a59
Oleksandr <vasa22292@gmail.com>
2024-02-17 19:37:13 +0100

    feat(api/routres/organisations): add get all organisations endpoint

--- client/api/routers/organisations.py
+++ client/api/routers/organisations.py
@@ -2,3 +2,34 @@ from sqlalchemy import exc
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.exc import IntegrityError
 from sqlmodel import Session, select, update
+
+from ..database import get_session
+from ..database.models import OrganisationMetadata, HotelMetadata
+from .commune import OrganisationFull, HotelBasic, OrganisationBasic, HotelFull
+
+router = APIRouter()
+
+@router.get("/")
+def get_all_organisations(
+    session: Session = Depends(get_session),
+) -> list[OrganisationBasic]:
+    try:
+        organisations = session.exec(select(OrganisationMetadata)).all()
+    except exc.NoResultFound:
+        raise HTTPException(
+            status_code=404,
+            detail="No Organisations Found",
+        )
+
+    # Convert organisations to OrganisationBasic model
+    organisations_list = [
+        OrganisationBasic(
+            title=organisation.title,
+            address=organisation.address,
+            subtitle=organisation.subtitle,
+            image=organisation.image,
+        )
+        for organisation in organisations
+    ]
+
+    return organisations_list
