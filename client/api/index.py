514c965975acfd17eac93ce14dd56dc37bad2d39
Oleksandr <vasa22292@gmail.com>
2024-02-17 19:35:05 +0100

    feat(api/indexer): add routers

--- client/api/index.py
+++ client/api/index.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from .routers import organisations, hotels, booking
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
+
+
+app.include_router(organisations.router, prefix="/api/organisations", tags=["organisations"])
+app.include_router(hotels.router, prefix="/api/hotels", tags=["hotels"])
+app.include_router(booking.router, prefix="/api/bookings", tags=["bookings"])
 
 @app.get("/api/python")
 def hello_world():
