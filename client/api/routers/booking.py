9c4558d0e43db438d25acdd46134a224669254e7
Oleksandr <vasa22292@gmail.com>
2024-02-17 19:37:51 +0100

    fix(api/routers/booking): depedencies

--- client/api/routers/booking.py
+++ client/api/routers/booking.py
@@ -2,4 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select, and_
 
+from ..database import get_session
+from ..database.models import Booking, HotelMetadata, Room
+from .commune import BookingBasic
+
 router = APIRouter()
