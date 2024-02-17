c3f97659714e2fee20f705cca1fb99e7bca19980
Oleksandr <vasa22292@gmail.com>
2024-02-17 22:02:53 +0100

    feat(api/routers/bookings): add get bookings from hotel endpoint

--- client/api/routers/booking.py
+++ client/api/routers/booking.py
@@ -7,3 +7,38 @@ from ..database.models import Booking, HotelMetadata, Room
 from .commune import BookingBasic
 
 router = APIRouter()
+
+@router.get("/hotel/{hotel_address}")
+def get_all_bookings_from_hotel(
+    hotel_address: str, session: Session = Depends(get_session)
+) -> list[BookingBasic]:
+    bookings = []
+
+    try:
+        # Fetch bookings for the given hotel
+        hotel_bookings = session.exec(
+            select(Booking).where(Booking.hotel == hotel_address)
+        ).all()
+
+        for hotel_booking in hotel_bookings:
+            # Fetch guest details
+            booking = BookingBasic(
+                bookingId=str(hotel_booking.booking_id),
+                hotel=hotel_booking.hotel,
+                roomId=str(hotel_booking.room_id),
+                cancellationDeadline=str(hotel_booking.cancellation_deadline),
+                price=str(hotel_booking.price),
+                guest=hotel_booking.guest,
+            )
+
+            bookings.append(booking)
+
+    except NoResultFound:
+        raise HTTPException(
+            status_code=404,
+            detail="Bookings Not Found for the specified hotel",
+        )
+
+    return bookings
+
+
