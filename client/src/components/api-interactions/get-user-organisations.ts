b04ba00bade66c0e2f1d0c21e5bd6fcbe88c6e49
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-02-18 10:03:43 +0100

    fix(client): get user orgs

--- client/src/components/api-interactions/get-user-organisations.ts
+++ client/src/components/api-interactions/get-user-organisations.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 import { OrganisationBasic } from '@/lib/types';
 
 export default function getUserOrganisations(userAddress: `0x${string}`) {
-  return axios.get<OrganisationBasic[]>(`/api/hotels/user_organisations/${userAddress}`);
+  return axios.get<OrganisationBasic[]>(`/api/organisations/user_organisations/${userAddress}`);
 }
