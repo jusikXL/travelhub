d0a3dd8cf2fd4cbe1f28d860cf1ad5bc97bd3a5a
Yevhenii <res_2005@ukr.net>
2024-02-18 01:28:39 +0100

    feat(api/routers): add get all user bookings and get all bookings of the room endpoints

--- client/api/routers/booking.py
+++ client/api/routers/booking.py
@@ -3,14 +3,15 @@ from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select, and_
 
 from ..database import get_session
-from ..database.models import Booking, HotelMetadata, Room
+from ..database.models import Booking
 from .commune import BookingBasic
 
 router = APIRouter()
 
+
 @router.get("/hotel/{hotel_address}")
 def get_all_bookings_from_hotel(
-    hotel_address: str, session: Session = Depends(get_session)
+        hotel_address: str, session: Session = Depends(get_session)
 ) -> list[BookingBasic]:
     bookings = []
 
@@ -42,3 +43,71 @@ def get_all_bookings_from_hotel(
     return bookings
 
 
+@router.get("/hotel/{hotel_address}/room/{room_id}")
+def get_all_bookings_in_room(
+        hotel_address: str, room_id: int, session: Session = Depends(get_session)
+) -> list[BookingBasic]:
+    bookings = []
+
+    try:
+        # Fetch bookings for the given room
+        room_bookings = session.exec(
+            select(Booking).where(
+                and_(Booking.hotel == hotel_address, Booking.room_id == room_id)
+            )
+        ).all()
+
+        for room_booking in room_bookings:
+            # Fetch guest details
+            booking = BookingBasic(
+                bookingId=str(room_booking.booking_id),
+                hotel=room_booking.hotel,
+                roomId=str(room_booking.room_id),
+                cancellationDeadline=str(room_booking.cancellation_deadline),
+                price=str(room_booking.price),
+                guest=room_booking.guest,
+            )
+
+            bookings.append(booking)
+
+    except NoResultFound:
+        raise HTTPException(
+            status_code=404,
+            detail="Bookings Not Found for the specified room",
+        )
+
+    return bookings
+
+
+@router.get("/user/{user_address}")
+def get_client_bookings(
+        user_address: str, session: Session = Depends(get_session)
+) -> list[BookingBasic]:
+    bookings = []
+
+    try:
+        # Fetch bookings for the given user
+        user_bookings = session.exec(
+            select(Booking).where(Booking.guest == user_address)
+        ).all()
+
+        for user_booking in user_bookings:
+            # Create Booking instance
+            booking = BookingBasic(
+                bookingId=str(user_booking.booking_id),
+                hotel=user_booking.hotel,
+                roomId=str(user_booking.room_id),
+                cancellationDeadline=str(user_booking.cancellation_deadline),
+                price=str(user_booking.price),
+                guest=user_booking.guest,
+            )
+
+            bookings.append(booking)
+
+    except NoResultFound:
+        raise HTTPException(
+            status_code=404,
+            detail="Bookings Not Found for the specified user",
+        )
+
+    return bookings
