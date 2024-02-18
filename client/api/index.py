6e0cec1d4b1120fd12a14f66b4ced691a5c94f6e
Yevhenii <res_2005@ukr.net>
2024-02-18 11:22:24 +0100

    Revert "refactor(api): add tabulation"
    This reverts commit 32d932c66bd081bd211e5b84e2030d4d795c5af6.

--- client/api/index.py
+++ client/api/index.py
@@ -2,10 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import organisations, hotels, booking
-
 app = FastAPI()
 
-# Sasha 𓀐╭ᑎ╮(‿ˠ‿)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,10 +13,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 app.include_router(organisations.router, prefix="/api/organisations", tags=["organisations"])
 app.include_router(hotels.router, prefix="/api/hotels", tags=["hotels"])
 app.include_router(booking.router, prefix="/api/bookings", tags=["bookings"])
-
 
 @app.get("/api/python")
 def hello_world():
